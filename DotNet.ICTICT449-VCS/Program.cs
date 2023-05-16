d38399641c55706f2e142c14cb014a0798251ac7
Douglas-Thornton <douglas.thornton@chamonix.com.au>
2023-05-16 14:08:27 +0930

    Basic code add.

--- DotNet.ICTICT449-VCS/Program.cs
+++ DotNet.ICTICT449-VCS/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DotNet.ICTICT449_VCS
@@ -10,6 +11,14 @@ namespace DotNet.ICTICT449_VCS
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Hello world!");
+			for(int i = 0; i < 3; i++) 
+			{ 
+			Console.Write(".");
+			Thread.Sleep(1000);
+			}
+			Console.WriteLine("Goodbye world!");
+			Thread.Sleep(5000);
 		}
 	}
 }
