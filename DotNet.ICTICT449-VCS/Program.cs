a7dd914aee40bbc07dee0724fea75335c8cee803
Douglas-Thornton <douglas.thornton@chamonix.com.au>
2023-05-24 14:56:28 +0930

    Another change!

--- DotNet.ICTICT449-VCS/Program.cs
+++ DotNet.ICTICT449-VCS/Program.cs
@@ -14,6 +14,7 @@ class Program
 
     // GitSweats made this change! 
 
+    // Douglas Thornton made this change!
 
     static void Main(string[] args) => new Program().MainAsync(args).GetAwaiter().GetResult();
 
